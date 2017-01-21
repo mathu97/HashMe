52613601bda27d4cdbec31f5b0f0b5572aec5858
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2017-01-21 17:52:31 -0500

    Works

---
+++ app/src/main/java/me/mathusan/hashme/ImageRecognizer.java
@@ -0,0 +1,97 @@
+package me.mathusan.hashme;
+
+import android.os.AsyncTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import android.os.AsyncTask;
+import android.view.View;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.request.ClarifaiRequest;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+
+public class ImageRecognizer {
+
+    // Provide your Client ID
+    private final static String CLIENT_ID = "Ps8k7AruPpKIXZBUSsQl-lqWPxQ2mh-07CAMtsQZ";
+
+    // Provider Your Client Secret Key
+    private final static String CLIENT_SECRET_KEY = "DdmH9xGFLRzoZdJcJY50Vec_eT_ykW9-nC4m2HUH";
+
+
+    // Defining List Object
+    private static List<String> resultList = new ArrayList<String>();
+
+    private static String path;
+
+    static ClarifaiClient client;
+
+    public static List<String> recognize(String imageUrl) {
+
+        path = imageUrl;
+
+        // Defining List Object
+        List<String> resultList = new ArrayList<String>();
+
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+
+            client = new ClarifaiBuilder(CLIENT_ID, CLIENT_SECRET_KEY).buildSync();
+
+            new Clarifaitask().execute();
+
+        }
+        return resultList;
+    }
+
+    private static class Clarifaitask extends AsyncTask<List<String>, String, List<String>>{
+
+
+        @Override
+        protected List<String> doInBackground(List<String>... params) {
+            final List<ClarifaiOutput<Concept>> predictionResults =
+                    client.getDefaultModels().generalModel() // You can also do client.getModelByID("id") to get custom models
+                            .predict()
+                            .withInputs(
+                                    ClarifaiInput.forImage(ClarifaiImage.of(new File(path)))
+                            )
+                            .executeSync()
+                            .get();
+
+            if (predictionResults != null && predictionResults.size() > 0) {
+
+                // Prediction List Iteration
+                for (int i = 0; i < predictionResults.size(); i++) {
+
+                    ClarifaiOutput<Concept> clarifaiOutput = predictionResults.get(i);
+
+                    List<Concept> concepts = clarifaiOutput.data();
+
+                    if(concepts != null && concepts.size() > 0) {
+                        for (int j = 0; j < concepts.size(); j++) {
+
+                            resultList.add(concepts.get(j).name());
+                        }
+                    }
+                }
+            }
+
+        return resultList;
+    }
+
+        @Override
+        protected void onPostExecute(List<String> resultList)
+        {
+            // Iteration of Result
+            for(String result : resultList) {System.out.println(result);}
+        }
+
+        }
+    }
--- app/src/main/java/me/mathusan/hashme/ViewActivity.java
+++ app/src/main/java/me/mathusan/hashme/ViewActivity.java
@@ -5,14 +5,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.StringRes;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -33,14 +30,10 @@ import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
-import clarifai2.api.ClarifaiResponse;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.input.image.ClarifaiImage;
-import clarifai2.dto.model.ConceptModel;
 import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Concept;
-
-
 import clarifai2.dto.prediction.Prediction;
 
 /**
@@ -49,89 +42,50 @@ import clarifai2.dto.prediction.Prediction;
 
 public class ViewActivity extends AppCompatActivity {
 
-    private static final String TAG = "TAG";
-    ClarifaiClient client;
     String path;
-    List<String> list = new ArrayList<>();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_view);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            Intent intent = getIntent();
-            Uri uri = intent.getParcelableExtra("uri");
-            path = intent.getStringExtra("path");
+        Intent intent = getIntent();
+        Uri uri = intent.getParcelableExtra("uri");
+        path = intent.getStringExtra("path");
 
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            ImageView imageView = (ImageView) findViewById(R.id.imageview);
+            imageView.setImageBitmap(bitmap);
 
-            client =
-                    new ClarifaiBuilder("Ps8k7AruPpKIXZBUSsQl-lqWPxQ2mh-07CAMtsQZ", "DdmH9xGFLRzoZdJcJY50Vec_eT_ykW9-nC4m2HUH").buildSync();
+            // List of Recognized Result from Image
+            List<String> resultList = ImageRecognizer.recognize(path);
 
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                ImageView imageView = (ImageView) findViewById(R.id.imageview);
-                imageView.setImageBitmap(bitmap);
-                new clarifaiTask().execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
             return true;
         }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-
-
-    class clarifaiTask extends AsyncTask<Void, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>> {
-
-
-        @Override
-        protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Void... params) {
-            // The default Clarifai model that identifies concepts in images
-            final ConceptModel generalModel = App.get().clarifaiClient().getDefaultModels().generalModel();
-
-            // Use this model to predict, with the image that the user just selected as the input
-            return generalModel.predict()
-                    .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(path)))
-                    .executeSync();
-            }
-
-        @Override
-        protected void onPostExecute(ClarifaiResponse<List<ClarifaiOutput<Concept>>> response) {
-            if (!response.isSuccessful()) {
-                System.out.println("Error While Contacting API");
-                return;
-            }
-            final List<ClarifaiOutput<Concept>> predictions = response.get();
-
-            if (predictions.isEmpty()) {
-                System.out.println("Empty");
-                return;
-            }
-            else
-            {
-
-            }//ADD TO LIST
-        }
-
-
-        }
-
+        return super.onOptionsItemSelected(item);
     }
+
+
+}
